246c3b30304de6b0fc47a3b6aba18cef40ecf4cb
Shamik Mondal <157884273+Shamik-Israfel@users.noreply.github.com>
2024-04-07 03:47:11 +0600

    @shamik just have done few things on leaverequest controller
    onky the tableview

--- src/Shamik_Mondal_2221145/HR_Manager/LeaveRequestController.java
+++ src/Shamik_Mondal_2221145/HR_Manager/LeaveRequestController.java
@@ -4,30 +4,66 @@
  */
 package Shamik_Mondal_2221145.HR_Manager;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  *
  * @author hp
  */
-public class LeaveRequestController {
+public class LeaveRequestController implements Initializable {
 
     @FXML
-    private TableView<?> leaveRequestsTableView;
+    private TableView<HRLeaveModel> leaveRequestsTableView;
     @FXML
-    private TableColumn<?, ?> codeTableColumn;
+    private TableColumn<HRLeaveModel,Integer> codeTableColumn;
     @FXML
-    private TableColumn<?, ?> nameTableColumn;
+    private TableColumn<HRLeaveModel,String> nameTableColumn;
     @FXML
-    private TableColumn<?, ?> departmentTableColumn;
+    private TableColumn<HRLeaveModel,String> departmentTableColumn;
     @FXML
-    private TableColumn<?, ?> leaveDaysTableColumn;
+    private TableColumn<HRLeaveModel, Integer> leaveDaysTableColumn;
     @FXML
-    private TableColumn<?, ?> reqStatusTableColumn;
+    private TableColumn<HRLeaveModel,String> reqStatusTableColumn;
 
+    private ObservableList<HRLeaveModel> reqlist;
+    private ObservableList<HRLeaveModel> updatereqlist;
+    
+    
+     @Override
+    public void initialize(URL location, ResourceBundle resources) {
+         reqlist=FXCollections.observableArrayList();
+        updatereqlist = FXCollections.observableArrayList();
+        
+         nameTableColumn.setCellValueFactory(new PropertyValueFactory<HRLeaveModel, String>("name"));  
+        codeTableColumn.setCellValueFactory(new PropertyValueFactory<HRLeaveModel, Integer>("employeecode"));
+        reqStatusTableColumn.setCellValueFactory(new PropertyValueFactory<HRLeaveModel, String>("status"));
+        departmentTableColumn.setCellValueFactory(new PropertyValueFactory<HRLeaveModel, String>("department"));
+        leaveDaysTableColumn.setCellValueFactory(new PropertyValueFactory<HRLeaveModel, Integer>("selectdays"));
+        
+        HRLeaveModel dummyIns = new HRLeaveModel("","","",0,0); 
+        
+        leaveRequestsTableView.getItems().addAll(reqlist);
+        
+    }
+    
+    private void refreshTable(){
+        // updatereqlist.clear();
+        leaveRequestsTableView.getItems().clear();
+        //updatereqlist = HR_Manager.acceptOrRejectPendingPermission(updatereqlist);//
+        leaveRequestsTableView.getItems().addAll(reqlist);
+    }
+    
+    
+    
     @FXML
     private void acceptButtonOnClicked(ActionEvent event) {
     }
@@ -40,4 +76,6 @@ public class LeaveRequestController {
     private void showTableButtonOnClicked(ActionEvent event) {
     }
  //Shamik   
+
+   
 }
