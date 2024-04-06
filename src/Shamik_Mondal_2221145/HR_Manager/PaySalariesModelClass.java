93af5fce0b9a7976c38eef002e7d7e5d806878e6
Shamik Mondal <157884273+Shamik-Israfel@users.noreply.github.com>
2024-04-07 02:58:09 +0600

    @Shamik Done PaySalariesModel

--- src/Shamik_Mondal_2221145/HR_Manager/PaySalariesModelClass.java
+++ src/Shamik_Mondal_2221145/HR_Manager/PaySalariesModelClass.java
@@ -4,10 +4,23 @@
  */
 package Shamik_Mondal_2221145.HR_Manager;
 
+import Shamik_Mondal_2221145.Employee;
+import java.io.Serializable;
+import java.time.LocalDate;
+
 /**
  *
  * @author hp
  */
-class PaySalariesModelClass {
+public class PaySalariesModelClass extends Employee implements Serializable {
+      protected int year;
+    protected long sal;
+    protected String mon;
     
+    public PaySalariesModelClass(int year, long sal, String mon, int employeeCode, String department, String address, LocalDate dateOfJoining, String name, String gender, String email, String userType, String password, LocalDate dateOfBirth, long contact) {
+        super(employeeCode, department, address, dateOfJoining, name, gender, email, userType, password, dateOfBirth, contact);
+        this.year = year;
+        this.sal = sal;
+        this.mon = mon;
+    }
 }
