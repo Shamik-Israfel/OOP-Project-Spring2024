baba630feefa5717979867cf0e18000dbc7da966
Shamik Mondal <157884273+Shamik-Israfel@users.noreply.github.com>
2024-04-08 03:17:36 +0600

    @Shamik Done with Financial Analysis Java class

---
+++ src/Shamik_Mondal_2221145/Account_Manager/FinancialAnalysis.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Shamik_Mondal_2221145.Account_Manager;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author hp
+ */
+public class FinancialAnalysis implements Serializable{
+    private double totalIncome;
+    private double totalExpenses;
+    private double netIncome;
+    
+}
