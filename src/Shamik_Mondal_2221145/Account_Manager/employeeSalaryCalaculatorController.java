fc493d9a5c4763c00b8c18bed9c50af626267c80
Shamik Mondal <157884273+Shamik-Israfel@users.noreply.github.com>
2024-04-08 00:40:56 +0600

    @Shamik Done with employeeSalaryCalculator
    without ReadWrite

--- src/Shamik_Mondal_2221145/Account_Manager/employeeSalaryCalaculatorController.java
+++ src/Shamik_Mondal_2221145/Account_Manager/employeeSalaryCalaculatorController.java
@@ -4,13 +4,19 @@
  */
 package Shamik_Mondal_2221145.Account_Manager;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
@@ -18,7 +24,7 @@ import javafx.scene.text.Text;
  *
  * @author hp
  */
-public class employeeSalaryCalaculatorController {
+public class employeeSalaryCalaculatorController implements Initializable {
 
     @FXML
     private TextField EmpIDTextField;
@@ -37,24 +43,70 @@ public class employeeSalaryCalaculatorController {
     @FXML
     private Text SalaryAmountShow;
     @FXML
-    private TableView<?> calcDetailsTableView;
+    private TableView<employeeSalaryDetails> calcDetailsTableView;
     @FXML
-    private TableColumn<?, ?> detailsNameTableColumn;
+    private TableColumn<employeeSalaryDetails, String> detailsNameTableColumn;
     @FXML
-    private TableColumn<?, ?> detailsCodeTableColumn;
+    private TableColumn<employeeSalaryDetails, Integer> detailsCodeTableColumn;
     @FXML
-    private TableColumn<?, ?> detailsTaxTableColumn;
+    private TableColumn<employeeSalaryDetails, Double> detailsTaxTableColumn;
     @FXML
-    private TableColumn<?, ?> detailsBonusTableColumn;
+    private TableColumn<employeeSalaryDetails, Integer> detailsBonusTableColumn;
     @FXML
-    private TableColumn<?, ?> detailsDeptTableColumn;
+    private TableColumn<employeeSalaryDetails, String> detailsDeptTableColumn;
+
+    private ObservableList<Salary> salaryList;
+    private ObservableList<Salary> salaryL;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        salaryL = FXCollections.observableArrayList();
+        salaryList = FXCollections.observableArrayList();
+
+        TaxPercentCombobox.getItems().addAll(5, 10, 15, 20);
+        DepartmentCombobox.getItems().addAll("Accounts", "President", "Coach", "Human Resource", "Player");
+
+        detailsNameTableColumn.setCellValueFactory(new PropertyValueFactory<employeeSalaryDetails, String>("name"));
+        detailsCodeTableColumn.setCellValueFactory(new PropertyValueFactory<employeeSalaryDetails, Integer>("code"));
+        detailsTaxTableColumn.setCellValueFactory(new PropertyValueFactory<employeeSalaryDetails, Double>("taxpercent"));
+        detailsBonusTableColumn.setCellValueFactory(new PropertyValueFactory<employeeSalaryDetails, Integer>("bonus"));
+        detailsDeptTableColumn.setCellValueFactory(new PropertyValueFactory<employeeSalaryDetails, String>("Department"));
+
+    }
 
     @FXML
     private void CalculateNetSalaryOnButtonClicked(MouseEvent event) {
+        int bonus = Integer.parseInt(BonusTextField.getText());
+        int salary = Integer.parseInt(SalaryAmountShow.getText());
+        int tax = TaxPercentCombobox.getValue();
+
+        Salary obj = new Salary(salary, bonus, tax, 0, "", "", null, "", "", "", "", "", null, 0);
+
+        salaryL.add(obj);
+        salaryList.add(obj);
+        int Total = 0;
+        for (Salary y : salaryL) {
+            Total += y.getNetSalary();
+        }
+        SalaryOutputTextArea.setText(Integer.toString(Total));
+        EmployeeNameShow.setText(NameTextField.getText());
     }
 
     @FXML
     private void departmentOnAction(ActionEvent event) {
+        String a = DepartmentCombobox.getValue();
+
+        if ("Accounts".equals(a)) {
+            SalaryAmountShow.setText("25000");
+        } else if ("President".equals(a)) {
+            SalaryAmountShow.setText("100000");
+        } else if ("Administration".equals(a)) {
+            SalaryAmountShow.setText("50000");
+        } else if ("Human Resource".equals(a)) {
+            SalaryAmountShow.setText("70000");
+        } else if ("Player".equals(a)) {
+            SalaryAmountShow.setText("30000");
+        }
     }
 
     @FXML
@@ -63,6 +115,21 @@ public class employeeSalaryCalaculatorController {
 
     @FXML
     private void detailsButtonOnClicked(ActionEvent event) {
+        String name = NameTextField.getText();
+        int code = Integer.parseInt(EmpIDTextField.getText());
+        double taxpercent = TaxPercentCombobox.getValue();
+        int bonus = Integer.parseInt(BonusTextField.getText());
+        String Department = DepartmentCombobox.getValue();
+
+        employeeSalaryDetails dummySalaryDetails = new employeeSalaryDetails(name, code, taxpercent, bonus, Department);
+
+        calcDetailsTableView.getItems().add(dummySalaryDetails);
+
+        NameTextField.clear();
+        EmpIDTextField.clear();
+        BonusTextField.clear();
+        TaxPercentCombobox.setValue(null);
+        DepartmentCombobox.setValue(null);
     }
-    
+
 }
--- src/Shamik_Mondal_2221145/Account_Manager/employeeSalaryDetails.java
+++ src/Shamik_Mondal_2221145/Account_Manager/employeeSalaryDetails.java
@@ -4,10 +4,74 @@
  */
 package Shamik_Mondal_2221145.Account_Manager;
 
+import java.io.Serializable;
+
 /**
  *
  * @author hp
  */
-public class employeeSalaryDetails {
+public class employeeSalaryDetails implements Serializable {
+    String name;
+    Integer code;
+    Double taxpercent;
+    Integer bonus;
+    String Department;
+
+   
+
+
+    public employeeSalaryDetails(String name, Integer code, Double taxpercent, Integer bonus, String Department) {
+        this.name = name;
+        this.code = code;
+        this.taxpercent = taxpercent;
+        this.bonus = bonus;
+        this.Department = Department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Double getTaxpercent() {
+        return taxpercent;
+    }
+
+    public void setTaxpercent(Double taxpercent) {
+        this.taxpercent = taxpercent;
+    }
+
+    public Integer getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Integer bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(String Department) {
+        this.Department = Department;
+    }
+
+    @Override
+    public String toString() {
+        return "employeeSalaryDetails{" + "name=" + name + ", code=" + code + ", taxpercent=" + taxpercent + ", bonus=" + bonus + ", Department=" + Department + '}';
+    }
+    
     
 }
