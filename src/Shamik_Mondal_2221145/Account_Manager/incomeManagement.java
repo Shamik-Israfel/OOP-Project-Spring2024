da541e777b7469824c95925cd2bb8c1e87bfd68f
Shamik Mondal <157884273+Shamik-Israfel@users.noreply.github.com>
2024-04-08 13:14:57 +0600

    @Shamik Done with incomeManagement java file
    time to go on with controller file

---
+++ src/Shamik_Mondal_2221145/Account_Manager/incomeManagement.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Shamik_Mondal_2221145.Account_Manager;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author hp
+ */
+
+public class incomeManagement implements Serializable{
+    private double sellingTickets;
+    private double sponsorDeals;
+    private double broadcastsRights;
+    private double totalIncome;
+    private double  sellingTicketsDistribution;
+    private double  sponsorDealsDistribution;
+    private double broadcastsRightsDistribution;
+    
+    
+ public incomeManagement(double sellingTickets, double sponsorDeals,
+                             double broadcastsRights) {
+        this.sellingTickets = sellingTickets;
+        this.sponsorDeals = sponsorDeals;
+        this.broadcastsRightsDistribution = broadcastsRightsDistribution;
+        
+        
+        
+        this.totalIncome = sellingTickets+sponsorDeals+broadcastsRights;
+        this.sellingTicketsDistribution=(sellingTickets/totalIncome)*100.0;
+        this.sponsorDealsDistribution=(sponsorDeals/totalIncome)*100.0;
+        this.broadcastsRightsDistribution=(broadcastsRights/totalIncome)*100.0;
+
+}
+
+    public double sellingTicketsDistribution() {
+        return sellingTicketsDistribution;
+    }
+
+    public double sponsorDealsDistribution() {
+        return sponsorDealsDistribution;
+    }
+
+    public double getbroadcastsRightsDistribution() {
+        return broadcastsRightsDistribution;
+    }
+
+    @Override
+public String toString() {
+    return "incomeManagement{" +
+            "\n    sellingTickets=" + sellingTickets +
+            ",\n    sponsorDeals=" + sponsorDeals +
+            ",\n    broadcastsRights=" + broadcastsRights +
+            ",\n    totalIncome=" + totalIncome +
+            ",\n    sellingTicketsDistribution=" + sellingTicketsDistribution +
+            ",\n    sponsorDealsDistribution=" + sponsorDealsDistribution +
+            ",\n    broadcastsRightsDistribution=" + broadcastsRightsDistribution +
+            "\n}";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Shamik_Mondal_2221145/Account_Manager/incomeManagementController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Shamik_Mondal_2221145.Account_Manager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author hp
+ */
+public class incomeManagementController {
+
+    @FXML
+    private TextField sellingTicketsTextArea;
+    @FXML
+    private TextField sponsorDealsTextArea;
+    @FXML
+    private TextField broadcastRightsTextArea;
+    @FXML
+    private Button showDetailsForTextAreaBUttonOnCick;
+    @FXML
+    private ComboBox<?> yearComboBox;
+    @FXML
+    private TextArea caluculatdAmountTextArea;
+    @FXML
+    private TextArea detailsTextArea;
+
+    @FXML
+    private void yearComboBoxButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void totalIncomeButttonOnClick(ActionEvent event) {
+    }
+    
+}
