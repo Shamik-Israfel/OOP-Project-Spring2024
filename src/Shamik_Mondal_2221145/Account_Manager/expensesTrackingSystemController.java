1a4804d825e58284885baedeaa1553caa368f55d
Shamik Mondal <157884273+Shamik-Israfel@users.noreply.github.com>
2024-04-08 17:32:07 +0600

    @Shamik Started the Journey for expenses tracking System
     Hope for the Best

---
+++ src/Shamik_Mondal_2221145/Account_Manager/expensesTrackingSystemController.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Shamik_Mondal_2221145.Account_Manager;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author hp
+ */
+public class expensesTrackingSystemController implements Initializable{
+
+    @FXML
+    private TextField amountUsedForFoodAndWatersupplyTextField;
+    @FXML
+    private TextField amountOfKitsExpenditureTextField;
+    @FXML
+    private TextField amountForTraininigSessionTextField;
+    @FXML
+    private TextField amountUsedForTotalSalariesTextField;
+    @FXML
+    private ComboBox<Integer> YearExpComboBox;
+    @FXML
+    private Text expTextArea;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @FXML
+    private void totalExpensesDetailsButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void expGeneratePieChartButtonOnClick(ActionEvent event) {
+    }
+
+    
+    
+}
