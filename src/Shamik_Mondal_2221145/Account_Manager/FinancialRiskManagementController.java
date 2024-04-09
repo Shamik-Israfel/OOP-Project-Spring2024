9a81ecd6a9a26665ec4eeab00e55f4e507adcc5c
Shamik Mondal <157884273+Shamik-Israfel@users.noreply.github.com>
2024-04-10 05:23:41 +0600

    @Shamik Started Working with Financial risk Management
    done with fxml and java model class Starting Progress on Controller Class

---
+++ src/Shamik_Mondal_2221145/Account_Manager/FinancialRiskManagementController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Shamik_Mondal_2221145.Account_Manager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
+/**
+ *
+ * @author hp
+ */
+public class FinancialRiskManagementController {
+
+    @FXML
+    private ComboBox<?> riskNameComboBox;
+    @FXML
+    private ComboBox<?> descripFinancialRiskComboBox;
+    @FXML
+    private ComboBox<?> riskProbabilityComboBox;
+    @FXML
+    private ComboBox<?> impactComboBox;
+    @FXML
+    private TextArea riskTextArea;
+
+    @FXML
+    private void generatePDFButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void showFinancialRiskButtonOnClick(ActionEvent event) {
+    }
+    
+}
